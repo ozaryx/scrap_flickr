73d45c38b319633b745931fcc5d6f1c130c7bcc3
Mankov Konstantin <k.mankov@cft.ru>
2019-02-19 18:21:50 +0700

    proxy

--- scraper2.py
+++ scraper2.py
@@ -12,25 +12,31 @@ search = input('Поиск: ')
 url = URL.format(search)
 
 proxies = {
-  'http': 'http://localhost:3128',
-  'https': 'http://localhost:3128',
+    'http': 'http://localhost:3128',
+    'https': 'http://localhost:3128',
 }
 
 print('Ищем изображения...')
-resp = requests.get(url, proxies=proxies)
+resp = requests.get(url)
+# resp = requests.get(url, proxies=proxies)
+# try:
+#     resp = requests.get(url)
+# except requests.exceptions.ProxyError:
+#     print('error', '')
 
 if resp.status_code != 200:
     print('error', resp.status_code)
-    
+
 doc = BS(resp.text, 'html.parser')
 
-# <div class="view photo-list-photo-view requiredToShowOnServer awake" 
-# style="transform: translate(551px, 359px); width: 249px; height: 166px; 
-# background-image: url(&quot;//c1.staticflickr.com/3/2222/2118049999_a05f4e8501.jpg&quot;);" 
-# id="yui_3_16_0_1_1550066883577_1181">  
+# <div class="view photo-list-photo-view requiredToShowOnServer awake"
+# style="transform: translate(551px, 359px); width: 249px; height: 166px;
+# background-image: url(&quot;//c1.staticflickr.com/3/2222/2118049999_a05f4e8501.jpg&quot;);"
+# id="yui_3_16_0_1_1550066883577_1181">
 # </div>
 
-foto_divs = doc.find_all('div', class_='view photo-list-photo-view requiredToShowOnServer awake')
+foto_divs = doc.find_all(
+    'div', class_='view photo-list-photo-view requiredToShowOnServer awake')
 
 regexp = r'<.*(//.*\.jpg).*>'
 regexp = re.compile(regexp)
